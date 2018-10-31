e34ad9396ffebda27ae1a91dab353844cf88b4a5
Alex <MyAlexEducation@gmail.com>
2018-10-31 12:25:29 +0300

    поведение для выпадашки

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,19 @@ overlay.addEventListener("click", function (evt) {
   overlay.classList.remove("overlay--on");
   cart_modal.classList.remove("modal-cart--on");
 })
+
+//поведение выпадашки
+var navigation = document.querySelector(".navigation");
+var navigation__switch = document.querySelector(".navigation__switch");
+navigation.classList.remove("navigation--nojs");
+navigation__switch.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if(navigation.classList.contains("navigation--closed")) {
+    navigation.classList.remove("navigation--closed");
+    navigation.classList.add("navigation--opened");
+  }
+  else {
+    navigation.classList.remove("navigation--opened");
+    navigation.classList.add("navigation--closed");
+  }
+})
